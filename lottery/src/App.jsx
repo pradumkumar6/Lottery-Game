1c02e6ed9cae414fde267af3e6fb007ea8353398
pradumkumar <pradumjaiswar790@gmail.com>
2024-02-13 10:01:45 +0530

    first commit

---
+++ lottery/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Lottery from './Lottery'
+import Ticket from './Ticket'
+
+function App() {
+
+  return (
+    <>
+     <Lottery n={3} winningSum={15}/>
+    </>
+  )
+}
+
+export default App
---
+++ lottery/src/Lottery.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { genTicket, sum } from "./helper";
+import Ticket from "./Ticket";
+export default function Lottery({ n=3, winningSum=15 }) {
+  let [ticket, setTicket] = useState(genTicket(n));
+  let isWinning = sum(ticket) === winningSum;
+  let buyTicket = () => {
+    setTicket(genTicket(n));
+  };
+  return (
+    <div>
+      <h1>Lottery Game!</h1>
+      <div className="ticket">
+        <Ticket ticket={ticket}/>
+      </div>
+      <br />
+      <button onClick={buyTicket}>Buy New Ticket</button>
+      <h3>{isWinning && "Congratulations you won😊"}</h3>
+    </div>
+  );
+}
